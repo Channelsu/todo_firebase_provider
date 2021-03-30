2b0af2132fa8b790f7c0b6ef57c028fabb96763e
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-03-30 22:06:25 +0900

    アプリのルートをChangeNotifierProviderで覆う

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todofirebaseprovider/pages/todo_list.dart';
+import 'package:todofirebaseprovider/services/todo_service.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,14 +10,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'todo_firebase_provider',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    // アプリのルートにTodoServiceを追加
+    return ChangeNotifierProvider<TodoService>(
+      create: (context) => TodoService(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'todo_firebase_provider',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: TodoList(),
       ),
-      home: TodoList(),
     );
   }
 }
