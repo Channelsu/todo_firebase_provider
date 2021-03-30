63bddf4464837eb4f1d7ae5c40df368bd52bd058
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-03-30 15:11:39 +0900

    todoモデル作成

---
+++ lib/models/todo.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class Todo {
+  String id;
+  String title;
+
+  Todo({@required this.title});
+}
+
+// データを保持する為のモデル
