c2abb51c65baad9aa963eb8d92ea96705bad5c3e
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-03-30 15:19:49 +0900

    todo一覧画面の土台作成

---
+++ lib/pages/todo_list.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemBuilder: (context, index) => ListTile(
+          title: Text('todo'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
