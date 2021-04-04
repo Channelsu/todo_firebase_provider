738bf1cdaa76c248f0ca1ea4bac11d8f6ef39a3c
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-04-04 18:20:42 +0900

    firebaseを使用してupdateTodoの内部処理作成

--- lib/services/todo_service.dart
+++ lib/services/todo_service.dart
@@ -35,11 +35,17 @@ class TodoService with ChangeNotifier {
     notifyListeners();  // UIの再描画
   }
 
-  updTodo(Todo todo) {
+  updTodo(Todo todo) async {
     // indexwhere : 引数に合致する最初のインデックスを返す
     var index = todos.indexWhere((el) => el.id == todo.id);
     // リストの中に対象のtodoが存在するかチェックし、あればそのtodoを更新する
-    if(index != -1) todos[index] = todo;
+    if(index != -1) {
+      // firebaseのtodoを更新
+      await firestore.collection('todos').document(todo.id).updateData({
+        'title': todo.title,
+      });
+      todos[index] = todo;
+    }
     notifyListeners();  // UIの再描画
   }
 }
